764ff0c5ca803b3abac3e5f8cb92cdb3871a55d8
Rohan Katwal <rohanwork909@gmail.com>
2025-07-14 14:50:46 +0545

    navigation

--- nextjs-demo/src/app/about/page.tsx
+++ nextjs-demo/src/app/about/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
 export default function About() {
+    const router=useRouter()
   return (
     <div>
       <h1>About Page</h1>
       <p>This is the about page of the Next.js demo.</p>
+      <button
+       onClick={()=>router.push("/")}
+       className="ml-4 p-4 bg-green-950 rounded-md">go home</button>
     </div>
   );
 }
--- nextjs-demo/src/app/layout.tsx
+++ nextjs-demo/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigation from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="bg-slate-900 text-red-400 text-center p-4">
-          <h1>Createing the rohan Next App</h1>
+          {/* <h1>Createing the rohan Next App</h1> */}
+          <Navigation/>
         </header>
         {children}
         <footer className="bg-slate-900 text-center p-4">
