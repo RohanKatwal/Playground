d2cf19f4c13ae281e74d660188dd96acd101af09
Rohan Katwal <rohanwork909@gmail.com>
2025-07-25 13:35:45 +0545

    code solve

--- Code Solve.js
+++ Code Solve.js
@@ -404,38 +404,81 @@ For that, we need to add a method to the String prototype:
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
-function longest(s1, s2) {
-  let combined=s1+s2;
-  let unique='';
+// function longest(s1, s2) {
+//   let combined=s1+s2;
+//   let unique='';
   
-  for(let i=0;i<combined.length;i++){
-    let exists=false;
+//   for(let i=0;i<combined.length;i++){
+//     let exists=false;
     
-    for(let j=0;j<unique.length; j++){
-      if(combined[i]===unique[j]){
-        exists=true;
-        break;
-      }
-    }
-    if(!exists){
-      unique+=combined[i]
-    }
+//     for(let j=0;j<unique.length; j++){
+//       if(combined[i]===unique[j]){
+//         exists=true;
+//         break;
+//       }
+//     }
+//     if(!exists){
+//       unique+=combined[i]
+//     }
+//   }
+//   let arr=unique.split('')
+//   for (i=0;i<arr.length;i++){
+//     for(j=i+1;j<arr.length;j++){
+//       if(arr[i]>arr[j]){
+//         let temp=arr[i]
+//         arr[i]=arr[j]
+//         arr[j]=temp
+//       }
+//     }
+//   }
+//   return arr.join('')
+// }
+
+// console.log(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq")); 
+// // → "abcdefklmopqwxy"
+
+
+// Q . You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+// Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+function longestConsec(strarr, k) {
+    // your code
+  if (strarr.length===0 || k>strarr.length ||k<=0){
+    return ""
   }
-  let arr=unique.split('')
-  for (i=0;i<arr.length;i++){
-    for(j=i+1;j<arr.length;j++){
-      if(arr[i]>arr[j]){
-        let temp=arr[i]
-        arr[i]=arr[j]
-        arr[j]=temp
-      }
+  let longest="";
+  for(let i=0; i<strarr.length-k;i++){//-k ensures we don’t go out of bounds.
+    let current=strarr.slice(i,k+i).join("")
+
+    if (current.length>longest.length){
+        longest=current
     }
+    
   }
-  return arr.join('')
+  return longest
 }
 
-console.log(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq")); 
-// → "abcdefklmopqwxy"
+console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+// → "folingtrashy"
+
 
 
 
