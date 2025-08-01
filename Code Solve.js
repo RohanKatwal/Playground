21dd9a63de269da23eddb890dfaf7b72249bbcf4
Rohan Katwal <rohanwork909@gmail.com>
2025-08-01 11:27:53 +0545

    code solve

--- Code Solve.js
+++ Code Solve.js
@@ -459,25 +459,52 @@ For that, we need to add a method to the String prototype:
 // longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
 // n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
 
-function longestConsec(strarr, k) {
-    // your code
-  if (strarr.length===0 || k>strarr.length ||k<=0){
-    return ""
-  }
-  let longest="";
-  for(let i=0; i<strarr.length-k;i++){//-k ensures we don’t go out of bounds.
-    let current=strarr.slice(i,k+i).join("")
+// function longestConsec(strarr, k) {
+//     // your code
+//   if (strarr.length===0 || k>strarr.length ||k<=0){
+//     return ""
+//   }
+//   let longest="";
+//   for(let i=0; i<=strarr.length-k;i++){//-k ensures we don’t go out of bounds.
+//     let current=strarr.slice(i,k+i).join("")
 
-    if (current.length>longest.length){
-        longest=current
-    }
+//     if (current.length>longest.length){
+//         longest=current
+//     }
     
+//   }
+//   return longest
+// }
+
+// console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+// // → "folingtrashy"
+
+
+// Q. There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+var number = function(busStops){
+  // Good Luck!
+  let people=0;
+  for(let i=0;i<busStops.length;i++){
+    people+=busStops[i][0]
+    people-=busStops[i][1]
   }
-  return longest
+  return people;
 }
 
-console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
-// → "folingtrashy"
+console.log(number([[10,0],[3,5],[5,8]])); // Output: 5
+
+
 
 
 
